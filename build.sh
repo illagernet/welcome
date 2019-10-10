d43750cd6a6114ecc076b4bfcc66dc30ec1d60b0
Benjamin Herman <benjaminherman97@gmail.com>
2019-10-10 16:44:15 -0700

    Fix welcome reminder

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+spigot='spigot-1.14.4.jar'
+name='Welcome'
+version='1.0.0'
+
 # Download Dependancies
 mkdir -p lib
-spigot='spigot-1.14.4.jar'
 if test ! -f lib/$spigot; then
     echo "Downloading Spigot"
     curl "https://cdn.getbukkit.org/spigot/$spigot" --output lib/$spigot
@@ -12,18 +15,17 @@ fi
 echo "Compiling plugin"
 rm -rf bin
 mkdir bin
-javac \
+javac $(find src -name "*.java") \
     -classpath lib/$spigot \
     -source 8 \
     -target 8 \
-    -d bin \
-    $(find src -name "*.java")
+    -d bin
 if [ $? -eq 1 ]; then
     exit 1
 fi
 
 # Archive plugin
 echo "Packaging plugin"
-jar -cvf Welcome.jar \
+jar -cvf $name-$version.jar \
     -C res . \
     -C bin .
--- src/net/illager/welcome/Welcome.java
+++ src/net/illager/welcome/Welcome.java
@@ -3,6 +3,9 @@ package net.illager.welcome;
 import java.util.List;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -14,7 +17,7 @@ public class Welcome extends JavaPlugin {
     
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
         this.saveDefaultConfig();
     }
     
@@ -23,44 +26,71 @@ public class Welcome extends JavaPlugin {
         // Do nothing
     }
     
-    /**
-     * Prepares a welcome kit
-     */
-    public ItemStack[] getKit() {
-        ItemStack totem = new ItemStack(Material.TOTEM_OF_UNDYING);
-        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
-        BookMeta meta = (BookMeta) book.getItemMeta();
-        this.reloadConfig();
-        meta.setTitle(this.getConfig().getString("welcome-book.title"));
-        meta.setAuthor(this.getConfig().getString("welcome-book.author"));
-        switch(this.getConfig().getInt("welcome-book.generation")) {
-            case 0:
-                meta.setGeneration(BookMeta.Generation.ORIGINAL);
-                break;
-            case 1:
-                meta.setGeneration(BookMeta.Generation.COPY_OF_ORIGINAL);
-                break;
-            case 2:
-                meta.setGeneration(BookMeta.Generation.COPY_OF_COPY);
-                break;
-            default:
-                meta.setGeneration(BookMeta.Generation.TATTERED);
+    // Player joins the server
+    class PlayerJoinListener implements Listener {
+        
+        @EventHandler
+        public void onPlayerJoin(PlayerJoinEvent event) {
+            Player player = event.getPlayer();
+            
+            // New player
+            if(!player.hasPlayedBefore()) {
+                
+                // Give kit items
+                player.getInventory().addItem(getKit());
+                
+                // Remind other players to welcome the new player
+                remindWelcome(player);
+            }
         }
-        for(String page : (List<String>) this.getConfig().getList("welcome-book.generation")) {
-            meta.addPage(page);
-        }
-        book.setItemMeta(meta);
-        ItemStack[] kit = { totem, book };
+    }
+    
+    // Prepares a welcome kit
+    ItemStack[] getKit() {
+        ItemStack[] kit = { new ItemStack(Material.TOTEM_OF_UNDYING), composeWelcomeBook() };
         return kit;
     }
     
-    /**
-     * Remind all other players to welcome a new player
-     * @param newPlayer the player being welcomed
-     */
-    public void remindWelcome(Player newPlayer) {
+    // Prepare a dynamic welcome book item
+    ItemStack composeWelcomeBook() {
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta meta = (BookMeta) book.getItemMeta();
+        
+        // Reload the plugin configuration file
+        this.reloadConfig();
+        
+        // Set metadata fields
+        meta.setTitle(this.getConfig().getString("welcome-book.title"));
+        meta.setAuthor(this.getConfig().getString("welcome-book.author"));
+        meta.setGeneration(this.getGeneration(this.getConfig().getInt("welcome-book.generation")));
+        
+        // Add pages
+        for(String page : (List<String>) this.getConfig().getList("welcome-book.generation")) {
+            meta.addPage(page);
+        }
+        
+        book.setItemMeta(meta);
+        return book;
+    }
+    
+    // Get a book generation from ID
+    BookMeta.Generation getGeneration(int generation) {
+        switch(generation) {
+            case 0: return BookMeta.Generation.ORIGINAL;
+            case 1: return BookMeta.Generation.COPY_OF_ORIGINAL;
+            case 2: return BookMeta.Generation.COPY_OF_COPY;
+            default: return BookMeta.Generation.TATTERED;
+        }
+    }
+    
+    // Remind other players to welcome the new player
+    void remindWelcome(Player newPlayer) {
+        
+        // For each player online
         for(Player player : this.getServer().getOnlinePlayers()) {
-            if(player.getUniqueId().toString() != newPlayer.getUniqueId().toString()) {
+            
+            // Not the new player
+            if(!player.getUniqueId().toString().equals(newPlayer.getUniqueId().toString())) {
                 player.sendMessage(ChatColor.GOLD + "Welcome " + ChatColor.WHITE + newPlayer.getDisplayName() + ChatColor.GOLD + " to the server!");
             }
         }
